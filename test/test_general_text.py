274c0975fd09d77e6f3f24ad507034a5577cd18f
zzy1127 <1726073424@qq.com>
2025-06-27 11:20:23 +0800

    update qurating filter

--- test/test_general_text.py
+++ test/test_general_text.py
@@ -1,23 +1,20 @@
-from dataflow.operators.process.GeneralText import NgramFilter, MinHashDeduplicator, QuratingFilter
-import pytest
-from dataflow.operators.refine.GeneralText import HtmlUrlRemoverRefiner
+from dataflow.operators.process.GeneralText import QuratingFilter
+
+
 from dataflow.utils.storage import FileStorage
-from dataflow.llmserving import APILLMServing_request, LocalModelLLMServing
 
 class TextPipeline():
-    def __init__(self, llm_serving=None):
-        
+    def __init__(self):
         self.storage = FileStorage(
-            first_entry_file_name="../dataflow/example/GeneralTextPipeline/pt_input.jsonl",
+            first_entry_file_name="./dataflow/example/GeneralTextPipeline/pt_input.jsonl",
             cache_path="./cache",
             file_name_prefix="dataflow_cache_step",
             cache_type="jsonl",
         )
-        
+        self.model_cache_dir = '/mnt/public/code/zzy/dataflow_cache'
         self.qrt = QuratingFilter()
-        
+
     def forward(self):
-        
         self.qrt.run(
             storage = self.storage.step(),
             input_key = "raw_content"
