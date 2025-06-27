5ee59008960727e2cc2f9974e81c3032b78b1210
Sunnyhaze <mxch1122@126.com>
2025-06-27 18:49:25 +0800

    [debug] find bad case conflict with import `XLMRobertaXLModel and `LocalModelLLMServing`

--- dataflow/operators/generate/Text2SQL/__init__.py
+++ dataflow/operators/generate/Text2SQL/__init__.py
@@ -2,7 +2,7 @@ from .DatabaseSchemaExtractor import DatabaseSchemaExtractor
 from .ExtraKnowledgeGenerator import ExtraKnowledgeGenerator
 from .PromptGenerator import PromptGenerator
 from .QuestionRefiner import QuestionRefiner
-from .SchemaLinking import SchemaLinking
+# from .SchemaLinking import SchemaLinking
 from .SQLDifficultyClassifier import SQLDifficultyClassifier
 from .SQLFilter import SQLFilter
 from .Text2SQLDifficultyClassifier import Text2SQLDifficultyClassifier
@@ -12,7 +12,7 @@ __all__ = [
     "ExtraKnowledgeGenerator",
     "PromptGenerator",
     "QuestionRefiner",
-    "SchemaLinking",
+    # "SchemaLinking",
     "SQLDifficultyClassifier",
     "SQLFilter",
     "Text2SQLDifficultyClassifier"
