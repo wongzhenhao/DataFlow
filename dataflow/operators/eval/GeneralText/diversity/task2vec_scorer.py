47ce9ed9a0bd46a47a0c472ee841a5fa394d6a17
zzy1127 <1726073424@qq.com>
2025-07-04 12:04:37 +0800

    modify logger and comment for diversity

--- dataflow/operators/eval/GeneralText/diversity/task2vec_scorer.py
+++ dataflow/operators/eval/GeneralText/diversity/task2vec_scorer.py
@@ -14,48 +14,39 @@ from typing import Optional
 @OPERATOR_REGISTRY.register()
 class Task2VecScorer(OperatorABC):
     def __init__(self, device='cuda', sample_nums=10, sample_size=1, method: Optional[str]='montecarlo', model_cache_dir='./dataflow_cache'):
-        self.sample_nums = sample_nums  # 样本数量
-        self.sample_size = sample_size  # 每次采样的样本大小
+        self.logger = get_logger()
+        self.logger.info(f'Initializing {self.__class__.__name__}...')
+        # evaluating diversity by extract sample_nums * sample_size samples
+        self.sample_nums = sample_nums  
+        self.sample_size = sample_size  
         self.device = device
         self.model_cache_dir = model_cache_dir  
         self.score_name = 'Task2VecScore'
         self.method = method
         if method not in ['montecarlo', 'variational']:
             raise ValueError(f"Invalid method '{method}'. Valid options are 'montecarlo' and 'variational'.")
-        self.logger = get_logger()
-
-        # 初始化 tokenizer 和模型
         self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2', cache_dir=self.model_cache_dir)
         self.probe_network = GPT2LMHeadModel.from_pretrained('gpt2', cache_dir=self.model_cache_dir)
-        
-        # 设置设备
         self.device = torch.device(self.device if self.device and torch.cuda.is_available() else "cpu")
         self.probe_network = self.probe_network.to(self.device)
+        self.logger.info(f'{self.__class__.__name__} initialized.')
 
     def preprocess(self, texts):
-        """ 预处理文本数据，将其转换为 token ID """
         self.tokenizer.pad_token = self.tokenizer.eos_token
         tokenized_outputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
         return {key: value.to(self.device) for key, value in tokenized_outputs.items()}
 
     def get_score(self, sentences):
-        """ 获取多样性评分 """
         embeddings = []
         data_length = len(sentences)
         for sample_num in range(self.sample_nums):
             self.logger.info(f'--> Sample {sample_num + 1}/{self.sample_nums}')
-
-            # 随机选取样本
             indices = random.sample(range(data_length), self.sample_size)
             texts = [sentences[i] for i in indices]
             tokenized_batch = self.preprocess(texts)
-
-            # 嵌入并计算 Task2Vec
             tokenized_dataset = CustomTensorDataset(tokenized_batch)
             embedding, _ = Task2Vec(self.probe_network, method=self.method).embed(tokenized_dataset)
             embeddings.append(embedding)
-
-        # 计算距离矩阵
         distance_matrix = task_similarity.pdist(embeddings, distance='cosine')
         div_coeff, conf_interval = task_similarity.stats_of_distance_matrix(distance_matrix)
         
@@ -64,20 +55,12 @@ class Task2VecScorer(OperatorABC):
             "ConfidenceInterval": conf_interval
         }
 
-    def eval(self, dataframe, input_key: str):
-        """ 从 DataFrame 获取文本并计算评分 """
-        samples = dataframe[input_key].to_list()
-        # 获取分数
-        task2vec_score = self.get_score(samples)
-        self.logger.info(f"Task2Vec Diversity Score: {task2vec_score}")
-        return task2vec_score
-
     def run(self, storage: DataFlowStorage, input_key: str):
-        """ 从 DataFlowStorage 读取数据并计算评分 """
         dataframe = storage.read("dataframe")
         samples = dataframe[input_key].to_list()
-        # 获取分数
+        self.logger.info(f"Evaluating {self.score_name}...")
         task2vec_score = self.get_score(samples)
+        self.logger.info("Evaluation complete!")
         self.logger.info(f"Task2Vec Diversity Score: {task2vec_score}")
         return task2vec_score
 
--- dataflow/operators/eval/GeneralText/diversity/vendi_scorer.py
+++ dataflow/operators/eval/GeneralText/diversity/vendi_scorer.py
@@ -10,33 +10,27 @@ from dataflow import get_logger
 @OPERATOR_REGISTRY.register()
 class VendiScorer(OperatorABC):
     def __init__(self, device='cuda'):
+        self.logger = get_logger()
+        self.logger.info(f'Initializing {self.__class__.__name__}...')
         self.bert_model_path = 'bert-base-uncased'
         self.simcse_model_path = 'princeton-nlp/unsup-simcse-bert-base-uncased'
         self.device = device
         self.score_name = 'VendiScore'
-        self.logger = get_logger()
+        self.logger.info(f'{self.__class__.__name__} initialized.')
 
     def get_score(self, sentences):
         result = {}
-        # ngram_vs = text_utils.ngram_vendi_score(sentences, ns=[1, 2, 3, 4])
-        # result["N-gramsVendiScore"] = round(ngram_vs, 2)
         bert_vs = text_utils.embedding_vendi_score(sentences, model_path=self.bert_model_path, device=self.device)
         result["BERTVendiScore"] = round(bert_vs, 2)
         simcse_vs = text_utils.embedding_vendi_score(sentences, model_path=self.simcse_model_path, device=self.device)
         result["SimCSEVendiScore"] = round(simcse_vs, 2)
         return result
-    
-    def eval(self, dataframe, input_key: str):
-        samples = dataframe[input_key].to_list()
-        # 获取分数
-        vendiscore = self.get_score(samples)
-        self.logger.info(f"VendiScore: {vendiscore}")
-        return vendiscore
 
     def run(self, storage: DataFlowStorage, input_key: str):
         dataframe = storage.read("dataframe")
         samples = dataframe[input_key].to_list()
-        # 获取分数
+        self.logger.info(f"Evaluating {self.score_name}...")
         vendiscore = self.get_score(samples)
+        self.logger.info("Evaluation complete!")
         self.logger.info(f"VendiScore: {vendiscore}")
         return vendiscore
