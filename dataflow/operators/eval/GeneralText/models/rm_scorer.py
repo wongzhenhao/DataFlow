2ccfdd537d709c8fd6e80ededf01b3c5c6ee5cf0
zzy1127 <1726073424@qq.com>
2025-07-04 13:10:28 +0800

    modify logger and comment for models

--- dataflow/operators/eval/GeneralText/models/rm_scorer.py
+++ dataflow/operators/eval/GeneralText/models/rm_scorer.py
@@ -9,35 +9,31 @@ from dataflow.utils.utils import get_logger
 @OPERATOR_REGISTRY.register()
 class RMScorer(OperatorABC):
     def __init__(self, device='cuda', model_cache_dir='./dataflow_cache', ):
+        self.logger = get_logger()
+        self.logger.info(f'Initializing {self.__class__.__name__}...')
         self.model_name = 'OpenAssistant/reward-model-deberta-v3-large-v2'
         self.model_cache_dir = model_cache_dir
         self.score_name = 'RewardModelScore'
         self.device = device
         self.rank_model = AutoModelForSequenceClassification.from_pretrained(self.model_name, cache_dir=self.model_cache_dir).to(self.device)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, cache_dir=self.model_cache_dir)
-        self.logger = get_logger()
-        self.logger.info(f"Using local model: {self.model_name}")
+        self.logger.info(f'{self.__class__.__name__} initialized.')
 
-        
     def eval(self, dataframe, input_instruction_key: str = 'instruction', input_output_key: str = 'output'):
         input_texts = dataframe.get(input_instruction_key, '').to_list()
         output_texts = dataframe.get(input_output_key, '').to_list()
         inputs = self.tokenizer(input_texts, output_texts, return_tensors='pt', padding=True, truncation=True).to(self.device)
-
+        self.logger.info(f"Evaluating {self.score_name}...")
         with torch.no_grad():
             logits = self.rank_model(**inputs).logits.cpu().detach().numpy()
-
         scores = logits.squeeze() 
-
         if scores.ndim == 0:  
             scores = [float(scores)]
-
+        self.logger.info("Evaluation complete!")
         return scores.tolist() 
 
     def run(self, storage: DataFlowStorage, input_instruction_key: str = 'instruction', input_output_key: str = 'output', output_key: str = 'RMScore'):
-        # Read the dataframe, evaluate scores, and store results
         dataframe = storage.read("dataframe")
-        self.logger.info(f"RMScorer ready to evaluate.")
         scores = self.eval(dataframe, input_instruction_key, input_output_key)
         dataframe[output_key] = scores        
         storage.write(dataframe)
