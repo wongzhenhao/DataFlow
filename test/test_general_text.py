5b0c032810b8fae1b85c186e11b194f822d91e33
zzy1127 <1726073424@qq.com>
2025-07-04 12:04:37 +0800

    modify logger and comment for diversity

--- test/test_general_text.py
+++ test/test_general_text.py
@@ -1,23 +1,22 @@
-from dataflow.operators.eval.GeneralText import CiderScorer 
+from dataflow.operators.eval.GeneralText import Task2VecScorer 
 from dataflow.utils.storage import FileStorage
 from dataflow.llmserving import APILLMServing_request
 import os
 class TextPipeline():
     def __init__(self):
         self.storage = FileStorage(
-            first_entry_file_name="./dataflow/example/GeneralTextPipeline/gen_input.jsonl",
+            first_entry_file_name="./dataflow/example/GeneralTextPipeline/pt_input.jsonl",
             cache_path="./cache",
             file_name_prefix="dataflow_cache_step",
             cache_type="jsonl",
         )
         self.model_cache_dir = './dataflow_cache'
-        self.scorer = CiderScorer()
+        self.scorer = Task2VecScorer()
 
     def forward(self):
         self.scorer.run(
             storage=self.storage.step(),
-            input_key='input_key',
-            reference_key='reference_key'
+            input_key='raw_content'
         )
 
 model = TextPipeline()
