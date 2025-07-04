3675df487540b4a5c49f5fc8c7df4639ae915ad2
zzy1127 <1726073424@qq.com>
2025-07-04 11:57:37 +0800

    modify logger and comment for gen and apicaller

--- dataflow/operators/eval/GeneralText/APIcaller/perspective_scorer.py
+++ dataflow/operators/eval/GeneralText/APIcaller/perspective_scorer.py
@@ -9,12 +9,12 @@ import os
 @OPERATOR_REGISTRY.register()
 class PerspectiveScorer(OperatorABC):
     def __init__(self, url: str='https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1'):
+        self.logger = get_logger()
+        self.logger.info(f'Initializing {self.__class__.__name__}...')
         self.api_key = os.environ.get('API_KEY')
         self.api_name = 'commentanalyzer'
         self.api_version = 'v1alpha1'
         self.discovery_service_url = url
-        self.logger = get_logger()
-        self.logger.info('Initializing Perspective API...')
         # must enable VPN
         self.client = discovery.build(
             'commentanalyzer',
@@ -23,14 +23,11 @@ class PerspectiveScorer(OperatorABC):
             discoveryServiceUrl=self.discovery_service_url,
             static_discovery=False,
         )
-        
         self.score_name = 'PerspectiveScore'
+        self.logger.info(f'{self.__class__.__name__} initialized.')
  
 
     def analyze_toxicity(self, text: str) -> float:
-        """
-        使用 Perspective API 分析文本的毒性得分
-        """
         analyze_request = {
             'comment': {'text': text},
             'requestedAttributes': {'TOXICITY': {}}
@@ -39,39 +36,28 @@ class PerspectiveScorer(OperatorABC):
         return response['attributeScores']['TOXICITY']['spanScores'][0]['score']['value']
 
     def get_score(self, samples, input_key: str) -> list:
-        """
-        为每个样本评估毒性分数
-        """
         scores = []
         for sample in samples:
             text = sample.get(input_key, '')
-            max_bytes = 20480  # 最大字节数限制
-            
+            max_bytes = 20480  # max byte
             if len(text.encode('utf-8')) > max_bytes:
                 text = text.encode('utf-8')[:max_bytes].decode('utf-8', errors='ignore')
-            
             score = self.analyze_toxicity(text)
             scores.append(score)
 
         return scores
 
     def eval(self, dataframe: pd.DataFrame, input_key: str) -> list:
-        """
-        为数据集中的每个样本批量评估毒性分数
-        """
-        # 将 dataframe 转换为字典列表
+        self.logger.info(f"Evaluating {self.score_name}...")
         samples = dataframe.to_dict(orient='records')
         scores = self.get_score(samples, input_key)
+        self.logger.info("Evaluation complete!")
         return scores
 
     def run(self, storage: DataFlowStorage, input_key: str, output_key: str = 'PerspectiveScore'):
-        """
-        运行算子，评估并将分数写入存储
-        """
         self.input_key = input_key
         self.output_key = output_key
         dataframe = storage.read("dataframe")
-        self.logger.info(f"PerspectiveScorer ready to evaluate.")
         scores = self.eval(dataframe, self.input_key)
         dataframe[self.output_key] = scores
         storage.write(dataframe)
--- dataflow/operators/eval/GeneralText/gen/cider_scorer.py
+++ dataflow/operators/eval/GeneralText/gen/cider_scorer.py
@@ -17,9 +17,10 @@ def load_idf(idf_path):
 class CiderScorer(OperatorABC):
     def __init__(self, n=4, sigma=6.0, df_mode="coco-val-df", idf_path="./dataflow/operators/eval/GeneralText/gen/cider/coco-val-df.p"):
         self.logger = get_logger()
+        self.logger.info(f'Initializing {self.__class__.__name__}...')
+        self.score_name = 'CiderScore'
         self.n = n  # Max n-gram length (default: 4)
         self.sigma = sigma  # Sigma for Gaussian penalty (default: 6.0)
-        
         self.df_mode = df_mode
         if self.df_mode != "corpus":
             # The idf file can be downloaded at https://github.com/ramavedantam/coco-caption/blob/master/data/coco-val-df.p
@@ -27,7 +28,7 @@ class CiderScorer(OperatorABC):
             self.idf = load_idf(idf_path)
         else:
             self.idf = None  # No need to load IDF for 'corpus' mode
-        
+        self.logger.info(f'{self.__class__.__name__} initialized.')
     
     def _score_func(self, eval_text, ref_text):
         cider_scorer = Cider(
@@ -37,7 +38,6 @@ class CiderScorer(OperatorABC):
             sigma=self.sigma,
             idf=self.idf  # Pass IDF (None if using 'corpus')
         )
-
         # Pass df_mode dynamically based on the argument
         cider_score, _ = cider_scorer.compute_score(df_mode='corpus' if self.idf is None else 'coco-val-df')  
         return cider_score
@@ -45,18 +45,16 @@ class CiderScorer(OperatorABC):
     def eval(self, dataframe, input_key, reference_key):
         eval_data = dataframe[input_key]
         ref_data = dataframe[reference_key]
-        
+        self.logger.info(f"Evaluating {self.score_name}...")
         scores = [self._score_func(eval_text, ref_text) for eval_text, ref_text in tqdm(zip(eval_data, ref_data), desc="CiderScorer Evaluating...")]
+        self.logger.info("Evaluation complete!")
         return scores
     
     def run(self, storage: DataFlowStorage, input_key: str, reference_key: str, output_key: str='CiderScore'):
         self.input_key = input_key
         self.reference_key = reference_key
         self.output_key = output_key
-        
         dataframe = storage.read("dataframe")
-        self.logger.info(f"CiderScore ready to evaluate.")
-        
         scores = self.eval(dataframe, input_key, reference_key)
         dataframe[self.output_key] = scores
         storage.write(dataframe)
