b53dcfdcca736ec96c6029c8163b4ba7be31dcdb
zzy1127 <1726073424@qq.com>
2025-07-04 14:05:31 +0800

    modify logger and comments for statistics

--- test/test_general_text.py
+++ test/test_general_text.py
@@ -1,21 +1,22 @@
-from dataflow.operators.eval.GeneralText import SuperfilteringScorer 
+from dataflow.operators.eval.GeneralText import LexicalDiversityScorer 
 from dataflow.utils.storage import FileStorage
 from dataflow.llmserving import APILLMServing_request
 import os
 class TextPipeline():
     def __init__(self):
         self.storage = FileStorage(
-            first_entry_file_name="./dataflow/example/GeneralTextPipeline/sft_input.jsonl",
+            first_entry_file_name="./dataflow/example/GeneralTextPipeline/pt_input.jsonl",
             cache_path="./cache",
             file_name_prefix="dataflow_cache_step",
             cache_type="jsonl",
         )
         self.model_cache_dir = './dataflow_cache'
-        self.scorer = SuperfilteringScorer()
+        self.scorer = LexicalDiversityScorer()
 
     def forward(self):
         self.scorer.run(
-            storage=self.storage.step()
+            storage=self.storage.step(),
+            input_key='raw_content'
         )
 
 model = TextPipeline()
