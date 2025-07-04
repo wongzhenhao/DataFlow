33e27c7a9ef9519a2f0d81497462eca9e0c3ff1a
zzy1127 <1726073424@qq.com>
2025-07-04 13:10:28 +0800

    modify logger and comment for models

--- dataflow/operators/eval/GeneralText/models/perplexity_scorer.py
+++ dataflow/operators/eval/GeneralText/models/perplexity_scorer.py
@@ -8,28 +8,28 @@ from dataflow.utils.utils import get_logger
 class PerplexityScorer(OperatorABC):
     # Need to download model first!
     def __init__(self, lang='en', model_name='dataflow/operators/eval/GeneralText/models/Kenlm/wikipedia'):
+        self.logger = get_logger()
+        self.logger.info(f'Initializing {self.__class__.__name__}...')
         self.model_name = model_name
         self.language = lang
         self.score_name = 'PerplexityScore'
-        self.logger = get_logger()
         try:
             self.model = KenlmModel.from_pretrained(self.model_name, self.language)
-            self.logger = get_logger()
-            self.logger.info(f"Using local model: {self.model_name}")
+            self.logger.info(f'{self.__class__.__name__} initialized.')
         except Exception as e:
             self.logger.error(f"Error loading model: {e}")
             self.logger.error("The model has not been downloaded yet.")
             self.logger.error("Please download the model from: https://huggingface.co/edugp/kenlm/tree/main")
             raise RuntimeError(f"Model loading failed. Please download the model from the provided link: https://huggingface.co/edugp/kenlm/tree/main. For default configuration, you can download en.arpa.bin, en.sp.model and en.sp.vocab, and put them in the folder dataflow/Eval/Text/models/Kenlm/wikipedia")
-
+        
     def eval(self, dataframe, input_key):
         input_texts = dataframe.get(input_key, '').to_list()
-
+        self.logger.info(f"Evaluating {self.score_name}...")
         results = []
         for text in input_texts:
             perplexity = self.model.get_perplexity(text)
             results.append(perplexity)
-        
+        self.logger.info("Evaluation complete!")
         return results
     
     def run(self, storage: DataFlowStorage, input_key: str = 'raw_content', output_key: str = 'PerplexityScore'):
--- test/test_general_text.py
+++ test/test_general_text.py
@@ -1,22 +1,21 @@
-from dataflow.operators.eval.GeneralText import Task2VecScorer 
+from dataflow.operators.eval.GeneralText import SuperfilteringScorer 
 from dataflow.utils.storage import FileStorage
 from dataflow.llmserving import APILLMServing_request
 import os
 class TextPipeline():
     def __init__(self):
         self.storage = FileStorage(
-            first_entry_file_name="./dataflow/example/GeneralTextPipeline/pt_input.jsonl",
+            first_entry_file_name="./dataflow/example/GeneralTextPipeline/sft_input.jsonl",
             cache_path="./cache",
             file_name_prefix="dataflow_cache_step",
             cache_type="jsonl",
         )
         self.model_cache_dir = './dataflow_cache'
-        self.scorer = Task2VecScorer()
+        self.scorer = SuperfilteringScorer()
 
     def forward(self):
         self.scorer.run(
-            storage=self.storage.step(),
-            input_key='raw_content'
+            storage=self.storage.step()
         )
 
 model = TextPipeline()
