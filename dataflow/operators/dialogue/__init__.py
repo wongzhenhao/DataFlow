b24ef47b192d154854a5e97a55b206b456994379
MOLYHECI <molyheci@outlook.com>
2025-07-11 23:04:49 +0800

    add FuncCallPipeline

---
+++ dataflow/operators/dialogue/__init__.py
@@ -0,0 +1,21 @@
+from .button_operator import (
+    ScenarioExtractor,
+    ScenarioExpander,
+    AtomicTaskGenerator,
+    SequentialTaskGenerator,
+    ParaSeqTaskGenerator,
+    CompositionTaskFilter,
+    FunctionGenerator,
+    MultiTurnDialogueGenerator
+)
+
+__all__ = [
+    "ScenarioExtractor",
+    "ScenarioExpander",
+    "AtomicTaskGenerator",
+    "SequentialTaskGenerator",
+    "ParaSeqTaskGenerator",
+    "CompositionTaskFilter",
+    "FunctionGenerator",
+    "MultiTurnDialogueGenerator"
+]
