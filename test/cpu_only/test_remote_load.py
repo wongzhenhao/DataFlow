aa7a79ffe9d332b548dcf5184bc20429ad3296bd
Zimo Meng <90973776+MOLYHECI@users.noreply.github.com>
2025-07-12 15:17:06 +0800

    add TYPE_CHECKING if-else for VSCode static check (#93)
    * add TYPE_CHECKING if-else for VSCode static check
    
    * remove remote load test for github actions

--- test/cpu_only/test_remote_load.py
+++ test/cpu_only/test_remote_load.py
@@ -40,7 +40,7 @@ class RemoteDataLoader():
             question_key = "question",
             answer_key = "answer"
         )
-@pytest.mark.cpu  
+@pytest.mark.gpu  
 def test_remote_data_loader():
     """
     Test function to run the RemoteDataLoader
