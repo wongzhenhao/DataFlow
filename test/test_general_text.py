7136282badd79d9f42057cc199d3203230f4262b
zzy1127 <1726073424@qq.com>
2025-07-12 14:47:31 +0800

    add consistent chat to conversations

--- test/test_general_text.py
+++ test/test_general_text.py
@@ -1,5 +1,7 @@
-from dataflow.operators.filter import LexicalDiversityFilter
+from dataflow.operators.conversations import ConsistentChatGenerator
 from dataflow.utils.storage import FileStorage
+from dataflow.serving import APILLMServing_request 
+
 
 class TextPipeline():
     def __init__(self):
@@ -9,13 +11,16 @@ class TextPipeline():
             file_name_prefix="dataflow_cache_step",
             cache_type="jsonl",
         )
+        serving = APILLMServing_request(
+            api_url="http://123.129.219.111:3000/v1/chat/completions",
+            model_name="gpt-4o"
+        )
         self.model_cache_dir = './dataflow_cache'
-        self.processor = LexicalDiversityFilter()
+        self.processor = ConsistentChatGenerator(llm_serving=serving, num_dialogs_per_intent=1)
 
     def forward(self):
         self.processor.run(
-            storage=self.storage.step(),
-            input_key='raw_content'
+            storage=self.storage.step()
         )
 
 model = TextPipeline()
