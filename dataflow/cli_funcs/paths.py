0ea386e93ab369d00e261eda7a383907bff4f060
Sunnyhaze <mxch1122@126.com>
2025-07-12 18:11:40 +0800

    [statics] add dataflow agnet for `dataflow init`

--- dataflow/cli_funcs/paths.py
+++ dataflow/cli_funcs/paths.py
@@ -34,6 +34,10 @@ class DataFlowPath:
         return DataFlowPath.get_dataflow_statics_dir() / "playground"
     
     @staticmethod
+    def get_dataflow_agentscript_dir():
+        return DataFlowPath.get_dataflow_statics_dir() / "dataflow_agent"
+    
+    @staticmethod
     def get_dataflow_agent_dir():
         return DataFlowPath.get_dataflow_dir() / "agent"
     
