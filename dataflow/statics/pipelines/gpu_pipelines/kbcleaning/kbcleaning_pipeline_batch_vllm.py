69223a2f5c392708a427bc025f53f545d5411cdb
ZhaoyangHan04 <3319926404@qq.com>
2025-07-22 23:07:09 +0800

    change kbc script in playground & manage kbc pipelines

---
+++ dataflow/statics/pipelines/gpu_pipelines/kbcleaning/kbcleaning_pipeline_batch_vllm.py
@@ -0,0 +1,70 @@
+from dataflow.operators.generate import (
+    CorpusTextSplitterBatch,
+    FileOrURLToMarkdownConverterBatch,
+    KnowledgeCleanerBatch,
+    MultiHopQAGeneratorBatch,
+)
+from dataflow.utils.storage import FileStorage
+from dataflow.serving import LocalModelLLMServing_vllm, LocalModelLLMServing_sglang
+
+
+class KBCleaning_batchvllm_GPUPipeline():
+    def __init__(self):
+
+        self.storage = FileStorage(
+            first_entry_file_name="../../example_data/KBCleaningPipeline/kbc_test.jsonl",
+            cache_path="./.cache/gpu",
+            file_name_prefix="batch_cleaning_step",
+            cache_type="json",
+        )
+
+        self.knowledge_cleaning_step1 = FileOrURLToMarkdownConverterBatch(
+            intermediate_dir="../../example_data/KBCleaningPipeline/raw/",
+            lang="en",
+            mineru_backend="vlm-sglang-engine",
+        )
+
+        self.knowledge_cleaning_step2 = CorpusTextSplitterBatch(
+            split_method="token",
+            chunk_size=512,
+            tokenizer_name="Qwen/Qwen2.5-7B-Instruct",
+        )
+
+    def forward(self):
+        self.knowledge_cleaning_step1.run(
+            storage=self.storage.step(),
+        )
+
+        self.knowledge_cleaning_step2.run(
+            storage=self.storage.step(),
+        )
+
+        self.llm_serving = LocalModelLLMServing_vllm(
+            hf_model_name_or_path="Qwen/Qwen2.5-7B-Instruct",
+            vllm_max_tokens=2048,
+            vllm_tensor_parallel_size=4,
+            vllm_gpu_memory_utilization=0.6,
+            vllm_repetition_penalty=1.2
+        )
+
+        self.knowledge_cleaning_step3 = KnowledgeCleanerBatch(
+            llm_serving=self.llm_serving,
+            lang="en"
+        )
+
+        self.knowledge_cleaning_step4 = MultiHopQAGeneratorBatch(
+            llm_serving=self.llm_serving,
+            lang="en"
+        )
+
+        self.knowledge_cleaning_step3.run(
+            storage=self.storage.step(),
+        )
+        self.knowledge_cleaning_step4.run(
+            storage=self.storage.step(),
+        )
+
+
+if __name__ == "__main__":
+    model = KBCleaning_batchvllm_GPUPipeline()
+    model.forward()
--- dataflow/statics/pipelines/gpu_pipelines/kbcleaning/kbcleaning_pipeline_url_sglang.py
+++ dataflow/statics/pipelines/gpu_pipelines/kbcleaning/kbcleaning_pipeline_url_sglang.py
@@ -5,20 +5,20 @@ from dataflow.operators.generate import (
     MultiHopQAGenerator,
 )
 from dataflow.utils.storage import FileStorage
-from dataflow.serving import LocalModelLLMServing_vllm
+from dataflow.serving import LocalModelLLMServing_sglang
 
-class KBCleaning_PDFvllm_GPUPipeline():
+class KBCleaning_URLSglang_GPUPipeline():
     def __init__(self):
 
         self.storage = FileStorage(
-            first_entry_file_name="../example_data/KBCleaningPipeline/kbc_placeholder.json",
+            first_entry_file_name="../../example_data/KBCleaningPipeline/kbc_placeholder.json",
             cache_path="./.cache/gpu",
-            file_name_prefix="pdf_cleaning_step",
+            file_name_prefix="url_cleaning_step",
             cache_type="json",
         )
 
         self.knowledge_cleaning_step1 = FileOrURLToMarkdownConverter(
-            intermediate_dir="../example_data/KBCleaningPipeline/raw/",
+            intermediate_dir="../../example_data/KBCleaningPipeline/raw/",
             lang="en",
             mineru_backend="vlm-sglang-engine",
         )
@@ -42,12 +42,11 @@ class KBCleaning_PDFvllm_GPUPipeline():
             output_key="raw_content",
         )
 
-        self.llm_serving = LocalModelLLMServing_vllm(
+        self.llm_serving = LocalModelLLMServing_sglang(
             hf_model_name_or_path="Qwen/Qwen2.5-7B-Instruct",
-            vllm_max_tokens=2048,
-            vllm_tensor_parallel_size=4,
-            vllm_gpu_memory_utilization=0.6,
-            vllm_repetition_penalty=1.2
+            sgl_dp_size=1, # data parallel size
+            sgl_tp_size=1, # tensor parallel size
+            sgl_max_new_tokens=1024,
         )
 
         self.knowledge_cleaning_step3 = KnowledgeCleaner(
@@ -72,5 +71,5 @@ class KBCleaning_PDFvllm_GPUPipeline():
         )
         
 if __name__ == "__main__":
-    model = KBCleaning_PDFvllm_GPUPipeline()
-    model.forward(raw_file="../example_data/KBCleaningPipeline/test.pdf")
+    model = KBCleaning_URLSglang_GPUPipeline()
+    model.forward(url="https://trafilatura.readthedocs.io/en/latest/quickstart.html")
