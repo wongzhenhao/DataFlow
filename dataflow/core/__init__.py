7789a3adf1cee1e04b289eccd1feae7d60f3823c
MOLYHECI <molyheci@outlook.com>
2025-07-25 01:21:34 +0800

    Add AutoOP for getting operator information & Add PipelineABC

--- dataflow/core/__init__.py
+++ dataflow/core/__init__.py
@@ -1,9 +1,12 @@
 from .Operator import OperatorABC, get_operator
 from .LLMServing import LLMServingABC
 from .Wrapper import WrapperABC
+from .Pipeline import PipelineABC
+
 __all__ = [
     'OperatorABC',
     'get_operator',
     'LLMServingABC',
     'WrapperABC',
+    'PipelineABC'
 ]
---
+++ test/test_autoop.py
@@ -0,0 +1,45 @@
+from dataflow.core import PipelineABC
+from dataflow.operators.filter import (
+    LLMLanguageFilter,
+)
+from dataflow.operators.eval import MetaScorer
+from dataflow.serving import APILLMServing_request
+from dataflow.utils.storage import FileStorage
+from dataflow.wrapper.auto_op import AutoOp
+
+class AutoOpPipeline(PipelineABC):
+    
+    def __init__(self):
+        
+        self.storage = FileStorage(
+            first_entry_file_name="./dataflow/example/GeneralTextPipeline/pt_input.jsonl",
+            cache_path="./cache",
+            file_name_prefix="dataflow_cache_step_langc",
+            cache_type="jsonl",
+        )
+        self.llm_serving = APILLMServing_request(
+            api_url="http://123.129.219.111:3000/v1/chat/completions",
+            model_name="gpt-4o"
+        )
+        self.llm_language_filter = LLMLanguageFilter(
+                llm_serving=self.llm_serving,
+                allowed_languages=['en']
+            )
+        self.meta_scorer = MetaScorer(llm_serving=self.llm_serving)    
+        
+    def forward(self):
+        self.llm_language_filter.run(
+            self.storage.step(),
+            input_key='raw_content'
+        )
+        self.meta_scorer.run(
+            self.storage.step(),
+            input_key='raw_content',
+        )
+        
+if __name__ == "__main__":
+    pipeline = AutoOpPipeline()
+    pipeline.autoop_register()
+    pipeline.forward()
+    print(AutoOp.op_runtimes)
+    # AutoOp.run_all() # Really run all operators
