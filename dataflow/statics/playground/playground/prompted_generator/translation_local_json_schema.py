f81150d78d1445e5f36865259efae5fd5b937fe6
root <zhenhao1141@gmail.com>
2025-10-09 16:21:37 +0800

    update statis example

--- dataflow/statics/playground/playground/prompted_generator/translation_local_json_schema.py
+++ dataflow/statics/playground/playground/prompted_generator/translation_local_json_schema.py
@@ -5,14 +5,14 @@ from dataflow.utils.storage import FileStorage
 class Qwen_generator():
     def __init__(self):
         self.storage = FileStorage(
-            first_entry_file_name="/mnt/DataFlow/wongzhenhao/DataFlow/dataflow/example/GeneralTextPipeline/translation.jsonl",
+            first_entry_file_name="../example_data/GeneralTextPipeline/translation.jsonl",
             cache_path="./cache",
             file_name_prefix="translation",
             cache_type="jsonl",
         )
         self.model_cache_dir = './dataflow_cache'
         self.llm_serving = LocalModelLLMServing_vllm(
-            hf_model_name_or_path="/mnt/DataFlow/models/Qwen2.5-7B-Instruct", # set to your own model path
+            hf_model_name_or_path="Qwen2.5-7B-Instruct", # set to your own model path
             vllm_tensor_parallel_size=1,
             vllm_max_tokens=8192,
         )
