9541c2d61bb799f78abe2b349a83477dc0bcd2b0
HeRunming <91403106+HeRunming@users.noreply.github.com>
2025-10-30 00:28:15 +0800

    Fix format problem of Reasoning Question Generator (#347)
    * [Fix Log] Fix log of import error of local vllm server
    
    * Translate error message into English
    
    * Implement response parsing for reasoning LLMs
    
    Add a method to parse responses from reasoning LLMs, extracting the answer from a specific format.Keep the question only.

--- dataflow/operators/reasoning/generate/reasoning_question_generator.py
+++ dataflow/operators/reasoning/generate/reasoning_question_generator.py
@@ -120,6 +120,15 @@ class ReasoningQuestionGenerator(OperatorABC):
 
         return formatted_prompts
 
+    def _parse_response(self, response: str):
+        # useful for reasoning llms. If response is in format of Deepseek thinking: <think>...</think><answer>...</answer>, keep only the answer part.
+        pattern = r"<think>(.*?)</think><answer>(.*?)</answer>"
+        matches = re.findall(pattern, response)
+        if matches:
+            return matches[0][1]
+        else:
+            return response
+    
     def run(
             self, 
             storage: DataFlowStorage, 
@@ -134,6 +143,7 @@ class ReasoningQuestionGenerator(OperatorABC):
         self._validate_dataframe(dataframe)
         formatted_prompts = self._reformat_prompt(dataframe)
         responses = self.llm_serving.generate_from_input(formatted_prompts)
+        responses = [self._parse_response(response) for response in responses]
 
         new_rows = pd.DataFrame({
             input_key: responses,
