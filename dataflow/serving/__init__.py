f6cf80fe875f397d8d94e3a6868b62d343055934
MOLYHECI <molyheci@outlook.com>
2025-11-26 16:25:19 +0800

    fix bug for dataflow/serving/__init__.py

--- dataflow/serving/__init__.py
+++ dataflow/serving/__init__.py
@@ -9,19 +9,20 @@ from .localhost_llm_api_serving import LocalHostLLMAPIServing_vllm
 from .localmodel_lalm_serving import LocalModelLALMServing_vllm
 
 from .LocalSentenceLLMServing import LocalEmbeddingServing
-
 from .light_rag_serving import LightRAGServing
+from .api_google_vertexai_serving import APIGoogleVertexAIServing
 
 from .local_model_vlm_serving import LocalVLMServing_vllm
 
 __all__ = [
+    "APIGoogleVertexAIServing",
     "APILLMServing_request",
     "LocalModelLLMServing_vllm",
     "LocalModelLLMServing_sglang",
     "APIVLMServing_openai",
     "PerspectiveAPIServing",
     "LiteLLMServing",
-    "LocalModelLALMServing_vllm"
+    "LocalModelLALMServing_vllm",
     "LocalHostLLMAPIServing_vllm",
     "LocalVLMServing_vllm",
 ]
